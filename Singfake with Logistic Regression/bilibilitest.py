b4e579a197f016f53b85505e23561b0dbcef64bd
Pryia <60443867+pryia10157@users.noreply.github.com>
2025-02-10 19:08:21 -0400

    Add files via upload

---
+++ Singfake with Logistic Regression/bilibilitest.py
@@ -0,0 +1,95 @@
+import asyncio
+from bilibili_api import video, Credential
+from bilibili_api import comment, sync
+import pandas as pd
+import time
+
+SESSDATA = ""
+BILI_JCT = ""
+BUVID3 = ""
+
+#BV1vP411m73H
+#https://www.bilibili.com/video/BV1vP411m73H/?spm_id_from=333.788
+
+def get_bvid(url):
+    
+    str_ar = url.split('/')
+    bvid_url = str_ar[4]
+    alt_id = bvid_url.split('?')
+    bvid_id = alt_id[0]
+    
+    return bvid_id
+
+def get_website(url):
+    
+    str_ar = url.split('/')
+    website = str_ar[2]
+    return website
+    
+
+def is_bilibili(str):
+    
+    if(str == "www.bilibili.com"):
+         return True
+    else:
+        return False
+
+
+    
+    
+async def get_stats(id,cred,i,df):
+    
+    await asyncio.sleep(1) 
+    
+    vid_data = video.Video(bvid=id, credential=cred)
+    
+    try:
+        vid_info = await vid_data.get_info()
+    except:
+        print("no data")
+        stats = [{'view': 0,'like':0,'reply':0}]
+    else:
+        stats = vid_info['stat']
+        df.loc[i,['views']] = stats['view']
+        df.loc[i,['likes']] = stats['like']
+        df.loc[i,['comments']] = stats['reply']
+        print(stats['view'])
+        
+        
+    return stats
+
+
+  
+    
+async def main():
+    
+    
+ df = pd.read_csv('newfile_updated_p3.csv')
+ cred = Credential(sessdata=SESSDATA, bili_jct=BILI_JCT, buvid3=BUVID3)
+ 
+ count = 0
+
+ 
+ for i in range(0, len(df)):
+     url = df.loc[i,"url"]
+     web = get_website(url)
+     
+         
+     if(is_bilibili(web) == True):
+            
+         id = get_bvid(url)
+         print(id)
+         await asyncio.sleep(1)
+         stats = await asyncio.gather(get_stats(id,cred,i,df))
+         print(stats)  
+         time.sleep(3)
+         print(df.loc[i])
+
+ df.to_csv('singfake_updated.csv', index=False)         
+         
+             
+    
+    
+
+
+asyncio.run(main())   
